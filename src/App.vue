326e5ed2f94dc9b4871362cbeeca7b14946b903a
Martins <martinszeltins@gmail.com>
2020-11-24 09:20:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    Hello world<br>
+    Why is this app making 400 HTTP requests to date-fns from dev server?
+</template>
+
+<script>
+import eo from 'date-fns/locale/eo'
+import { formatWithOptions } from 'date-fns/fp'
+
+export default {
+    setup() {
+        const dateToString = formatWithOptions({ locale: eo }, 'yyyy-mm-dd')
+    }
+}
+</script>
